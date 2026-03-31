38292f5c5f1c7aba8031d20d1546408ae78a98d4
Karine Louise <128435308+karinelouise@users.noreply.github.com>
2026-03-31 22:55:28 +0000

    test_hello_fail

--- tests/appTest.py
+++ tests/appTest.py
@@ -10,3 +10,14 @@ class AppTest(unittest.TestCase):
         self.assertEqual(200, response.status_code, "Erro no test_http_code!")
         self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
                           , "Erro no test_print_health_check!")
+
+        def test_hello_fail(self):
+        response = self.app.get("/hello")
+        self.assertEqual(400, response.status_code, "Erro no test_http_code!")
+        self.assertEqual(
+            "Nome não informado",
+            response.get_data(as_text=True),
+            "Erro no test_hello_fail!",
+        )
+
+        
